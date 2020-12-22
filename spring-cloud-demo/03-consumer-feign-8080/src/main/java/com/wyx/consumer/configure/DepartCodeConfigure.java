722699a7316cd4f812a73b26d141648c2a014500
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-12-22 14:22:24 +0800

    add 负载均衡

--- spring-cloud-demo/03-consumer-feign-8080/src/main/java/com/wyx/consumer/configure/DepartCodeConfigure.java
+++ spring-cloud-demo/03-consumer-feign-8080/src/main/java/com/wyx/consumer/configure/DepartCodeConfigure.java
@@ -1,17 +1,31 @@
 package com.wyx.consumer.configure;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import com.wyx.consumer.balance.CustomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DepartCodeConfigure {
+    /**
+     * 指定ribbon使用随机算法策略
+     * 如果 xml 与 Configure 文件中都设置了策略，
+     * Configure这里的设置的优先级更高
+     */
+//    @Bean
+//    public IRule loadBalanceRule() {
+//        return new RoundRobinRule();
+//    }
 
-    // 负载均衡
-    @LoadBalanced
+    // 修改负载均衡策略为：自定义策略
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public IRule loadBalanceRule() {
+        List<Integer> excludePorts = new ArrayList<>();
+        excludePorts.add(8083);
+        return new CustomRule(excludePorts);
     }
 }
---
+++ spring-cloud-demo/03-provider-8084/src/main/java/com/wyx/provider/Provider8084Application.java
@@ -0,0 +1,15 @@
+package com.wyx.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @EnableEurekaClient     // 仅限于注册中心是Eureka(只有 springboot项目需要加)
+// @EnableDiscoveryClient  // 注册中心可以是任意的类型(只有 springboot项目需要加)
+@SpringBootApplication
+public class Provider8084Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Provider8084Application.class, args);
+	}
+
+}
