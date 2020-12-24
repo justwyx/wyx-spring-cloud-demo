50cc0e5e8632a082a37020efe35b8387391c37ff
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-12-24 11:26:32 +0800

    add:集群分组仪表盘

---
+++ spring-cloud-demo/04-consumer-turbine-client-8680/src/main/java/com/wyx/consumer/ApplicationConsumerTurbineClient8680.java
@@ -0,0 +1,16 @@
+package com.wyx.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients // 开启Feign客户端
+//@EnableCircuitBreaker // 开启熔断器
+//@SpringBootApplication
+@SpringCloudApplication // 包含了上面两个注解
+public class ApplicationConsumerTurbineClient8680 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConsumerTurbineClient8680.class, args);
+	}
+}
